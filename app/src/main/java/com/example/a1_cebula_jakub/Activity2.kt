9fe64985670e94ffd03648f1e00fabf48132de80
ShadowproofGamer <57487079+ShadowproofGamer@users.noreply.github.com>
2023-10-23 19:39:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a1_cebula_jakub/Activity2.kt
@@ -0,0 +1,11 @@
+package com.example.a1_cebula_jakub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
